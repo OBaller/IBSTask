df12291c9e51604f2556b5f03210f97b6a032755
Naseem Oyebola <naseemoyebola@gmail.com>
2022-09-30 12:42:51 +0100

    initial commit

---
+++ IBSTask/IBSTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IBSTask
+//
+//  Created by Naseem Oyebola on 30/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IBSTask/IBSTask/IBSTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBSTaskApp.swift
+//  IBSTask
+//
+//  Created by Naseem Oyebola on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct IBSTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
