ec2c8567563529518e81f652f179e6a854c32a9e
Naseem Oyebola <naseemoyebola@gmail.com>
2022-10-09 11:14:08 +0100

    Edit API service

--- IBSTask/IBSTask/NetworkService/APIService.swift
+++ IBSTask/IBSTask/NetworkService/APIService.swift
@@ -11,13 +11,37 @@ import SwiftUI
 
 class APIService {
     static let shared = APIService()
-    func getUsers() -> AnyPublisher<UserDetails, Error> {
-        let components = URLComponents(string: "https://randomuser.me/api")!
-        let request = URLRequest(url: components.url!, timeoutInterval: 5)
-        return URLSession.shared.dataTaskPublisher(for: request)
-            .map(\.data)
-//            .decode(type: [User].self, decoder: JSONDecoder())
-            .decode(type: UserDetails.self, decoder: JSONDecoder())
-            .eraseToAnyPublisher()
+    var cancellables = Set<AnyCancellable>()
+    
+    public enum APIError: Error {
+        case error(_ errorString: String)
+    }
+    
+   
+    
+    public func getJSON<T: Decodable>(urlString: String, completion: @escaping(Result<T, APIError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.error(NSLocalizedString("Error: Invalid url", comment: ""))))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        let decoder = JSONDecoder()
+        
+       URLSession.shared.dataTaskPublisher(for: request)
+            .map { $0.data }
+            .decode(type: T.self, decoder: decoder)
+            .receive(on: RunLoop.main)
+            .sink { (taskCompletion) in
+                switch taskCompletion {
+                case .finished:
+                    return
+                case .failure(let decodingError):
+                    completion(.failure(.error("Error: \(decodingError.localizedDescription)")))
+                }
+            } receiveValue: { (decodedData) in
+                completion(.success(decodedData))
+            }
+            .store(in: &cancellables)
     }
 }
