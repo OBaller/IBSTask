e36cd60c7c52ede19fd5bdc0e16b01ed7b5e4db4
Naseem Oyebola <naseemoyebola@gmail.com>
2022-10-10 09:10:09 +0100

    extend user view model

--- IBSTask/IBSTask/ViewModel/UserViewModel.swift
+++ IBSTask/IBSTask/ViewModel/UserViewModel.swift
@@ -9,27 +9,61 @@ import Foundation
 import Combine
 
 class UserViewModel: ObservableObject {
-//    @Published var users: [UserDetails] = []
-    @Published var users: UserDetails?
+    @Published var personDataObjectList: [UserDetails] = []
+    @Published var searchResults: [UserDetails] = []
+    @Published var apiError: ApiError? = nil
+    @Published var isLoading: Bool = false
+    @Published var searchTerm = ""
+    var tempPersonDataStore: [UserDetails] = []
 
+    init() {
+        self.isLoading = true
+        for _ in (0 ... 2) {
+            getPersonData()
+        }
+        filterPersonDataList()
+    }
 
-    private var cancellable: AnyCancellable?
+    func stopActivityIndicatorLoading() {
+        if self.personDataObjectList.count == 3 {
+            self.isLoading = false
+        }
+    }
     
-//    func getUsers() {
-////        cancellable = APIService.shared.getUsers()
-//            .receive(on: DispatchQueue.main)
-//            .sink { completion in
-//                switch completion {
-//                case .failure(let error):
-////                    self.isRequestFailed = true
-//                    print(error)
-//                case .finished:
-//                    print("finished loading")
-//                }
-//            } receiveValue: { users in
-//                print(users)
-//                self.users = users
-//            }
-//    }
+    func getPersonData() {
+        let apiService: APIService = APIService.shared
+       
+        apiService.getJSON(urlString: "https://randomuser.me/api", completion: { (result: Result<User, APIService.APIError>) in
+            switch result {
+                
+            case .success(let person):
+                DispatchQueue.main.async {
+                    self.personDataObjectList.append(contentsOf: person.results)
+                    self.stopActivityIndicatorLoading()
+                }
+            case .failure(let apiError):
+                switch apiError {
+                case .error(let errorString):
+                    DispatchQueue.main.async {
+                        self.isLoading = false
+                        self.apiError = ApiError(errorString: errorString)
+                    }
+                }
+            }
+        })
+    }
+    
+    func filterPersonDataList() {
+        $searchTerm
+            .map { searchTerm in
+                self.personDataObjectList.filter({ person in
+                    let fullName =
+                    (person.name?.first.lowercased() ?? "") + (person.name?.last.lowercased() ?? "")
+                    return fullName.contains(searchTerm.lowercased())
+
+                })
+            }
+            .assign(to: &$searchResults)
+    }
     
 }
